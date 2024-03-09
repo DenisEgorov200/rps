6cddad11697aa51db5168b24096a8062256b402f
DenisEgorov200 <egorov.denis.mem@gmail.com>
2024-03-09 21:06:39 +0300

    feat: add responsive in nav menu

--- src/layout/ui/nav-menu/ui/nav-menu.tsx
+++ src/layout/ui/nav-menu/ui/nav-menu.tsx
@@ -3,11 +3,11 @@ const NAV_MENU_ITEMS = ['сундуки', 'арены', 'скины', 'бону�
 export const NavMenu = () => {
   return (
     <nav className="fixed bottom-0 w-full sm:relative">
-      <ul className="flex items-center sm:px-32">
+      <ul className="flex items-center sm:px-24">
         {NAV_MENU_ITEMS.map((item) => (
           <li
             key={item}
-            className="w-full p-6 bg-gradient-blue shadow-nav-menu rounded-t text-sm text-white text-center capitalize cursor-pointer sm:rounded-lg transition-all duration-300 hover:bg-gradient-blue-active hover:shadow-nav-menu-active"
+            className="w-full py-5 sm:p-6 bg-gradient-blue shadow-nav-menu rounded-t sm:rounded-lg text-sm sm:text-lg text-white text-center capitalize cursor-pointer transition-all duration-300 hover:bg-gradient-blue-active hover:shadow-nav-menu-active hover:scale-y-105"
           >
             {item}
           </li>
