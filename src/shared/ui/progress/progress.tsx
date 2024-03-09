4efb7cb0ae6dfb2a2a7974f4a65b92abd55469ee
DenisEgorov200 <egorov.denis.mem@gmail.com>
2024-03-09 16:06:34 +0300

    feat: add progress

---
+++ src/shared/ui/progress/progress.tsx
@@ -0,0 +1,20 @@
+interface Props {
+  progress: string
+}
+
+export const Progress = ({ progress }: Props) => {
+  return (
+    <div className="relative flex items-center justify-center w-1/2 max-w-40 bg-black rounded-[1px] text-[7px] font-medium text-white text-center">
+      <div
+        className="absolute z-10 top-0 left-0 h-full max-w-full bg-blue-500 rounded-[1px]"
+        style={{ width: progress }}
+      />
+      <span className="relative z-20 py-0.5 leading-none">100/200</span>
+      <img
+        src="/icons/star.svg"
+        alt="star"
+        className="absolute top-50 -right-2 z-10"
+      />
+    </div>
+  )
+}
